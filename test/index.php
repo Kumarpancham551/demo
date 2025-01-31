dd3534c98a89f06f14711da4876670fe93945547
Pancham <pancham.kumar@navgrahaa.com>
2025-01-31 10:27:31 +0530

    latest chnages

--- test/index.php
+++ test/index.php
@@ -1,5 +1,6 @@
 <?php 
 
-echo "Git testing on server";
-echo "I also want to deploy next proj"
+echo " Git testing on server";
+echo " I also want to deploy next proj";
+echo " I also want to deploy node proj";
 ?>
